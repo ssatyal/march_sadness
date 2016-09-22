9e2a02290e92e2b398d1a63127258a41c1becb10
Sanjaya Satyal <sanjaya.k.satyal@gmail.com>
2016-09-22 16:35:40 -0400

    seed data for 2016, sample for 2015

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,276 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Year.destroy_all
+Lookslike.destroy_all
+
+sixteen = Year.create!(year: 2016)
+fifteen = Year.create!(year: 2015)
+
+fifteen.lookslikes.create!(name: "Stugotz", looks_like: "Looks very sick.", photo_url: "http://www.adweek.com/tvnewser/wp-content/uploads/sites/3/2015/11/stugotz.png")
+
+
+sixteen.lookslikes.create!(name: 'Jim Caldwell',
+looks_like: 'Looks like the nervous armored car driver transporting his largest and last delivery, suspicious his new partner might be up to something. Jay Cutler is his new partner.',
+photo_url: 'http://i.imgur.com/lf9MJYM.jpg')
+
+sixteen.lookslikes.create!(name: 'Jay Cutler',
+looks_like: 'Looks like the guy trying to break into a house on an alarm company commercial.',
+photo_url: 'http://i.imgur.com/9g1bKdR.jpg')
+
+sixteen.lookslikes.create!(name: 'Tom Thibodeau',
+looks_like: 'Looks like the guy in an antacid commercial uncomfortably eating a chili dog while pulling at his collar for relief.',
+photo_url: 'http://i.imgur.com/xYNkGVV.jpg')
+
+sixteen.lookslikes.create!(name: 'Wade Phillips',
+looks_like: 'Looks like the Bank Security guard who gives up his gun after robbers get a jump on him by putting a banana to his neck.',
+photo_url: 'http://i.imgur.com/QqVKb4m.jpg')
+
+sixteen.lookslikes.create!(name: 'Paul Finebaum',
+looks_like: 'Looks like the human version of the NCAA rule book.',
+photo_url: 'http://i.imgur.com/RA6pj6R.jpg')
+
+sixteen.lookslikes.create!(name: 'Jim Caldwell',
+looks_like: 'Looks like the guy answering the door at 3 am who instantly realizes the policeman there is going to give him life-changing bad news.',
+photo_url: 'http://i.imgur.com/yovQAgV.jpg')
+
+sixteen.lookslikes.create!(name: 'Pete Carroll',
+looks_like: 'Face looks like a mask a bank robber would wear.',
+photo_url: 'http://i.imgur.com/85Zvuu2.jpg')
+
+sixteen.lookslikes.create!(name: 'Bartolo Colon',
+looks_like: 'Looks like the last Mohican, who ate the second to last Mohican.',
+photo_url: 'http://i.imgur.com/A39m9ss.jpg')
+
+sixteen.lookslikes.create!(name: 'Joe Torre',
+looks_like: 'Looks like the guy who enters the sauna wearing a gold chain and takes off his towel in the middle of the sauna while walking towards the open bench across from you, and sits down next to Wade Phillips.',
+photo_url: 'http://i.imgur.com/7gcolQT.jpg')
+
+sixteen.lookslikes.create!(name: 'Wade Phillips',
+looks_like: "Looks like a guy who can't figure out how the critters are getting under the fence.",
+photo_url: 'http://i.imgur.com/RNQ4L8V.jpg')
+
+sixteen.lookslikes.create!(name: 'Steve Kerr',
+looks_like: 'Looks like the guy who starts practicing his golf swing mid-conversation.',
+photo_url: 'http://i.imgur.com/AtbwRzh.jpg')
+
+sixteen.lookslikes.create!(name: 'Greg Olsen',
+looks_like: 'Looks like He-Man if he moved to Portland and opened a store that sells artisanal jams.',
+photo_url: 'http://i.imgur.com/jc7nUcq.jpg')
+
+sixteen.lookslikes.create!(name: 'Jay Gruden',
+looks_like: 'Looks like a flashy local realtor whose photo on the bus stop seat keeps getting defaced with horns and a mustache.',
+photo_url: 'http://i.imgur.com/PrwsoEA.jpg')
+
+sixteen.lookslikes.create!(name: 'Lane Kiffin',
+looks_like: 'Is the boater who throttles up in a no wake zone because he sees Jim Tomsula fishing with a Snoopy pole in a row boat.',
+photo_url: 'http://i.imgur.com/vd42VWZ.jpg')
+
+sixteen.lookslikes.create!(name: 'Brian Dawkins',
+looks_like: 'Looks like the guy at church who points to the sky with his eyes closed biting his lip and crying.',
+photo_url: 'http://i.imgur.com/CP7VWdKm.jpg')
+
+sixteen.lookslikes.create!(name: 'Matthew Stafford ',
+looks_like: 'Looks like your friend\'s friend who uses your bathroom, comes out fifteen minutes later and asks, "Do you have a plunger?"',
+photo_url: 'http://i.imgur.com/qH4EntP.jpg')
+
+sixteen.lookslikes.create!(name: 'Ty Lawson',
+looks_like: 'Looks like the DirecTV installer who shows up to your house an hour late and claims he has the wrong equipment and has to reschedule.',
+photo_url: 'http://i.imgur.com/IFjg3sJ.jpg')
+
+sixteen.lookslikes.create!(name: 'Russell Wilson ',
+looks_like: 'Looks like a dolphin trainer.',
+photo_url: 'http://i.imgur.com/HGVnHIQ.jpg')
+
+sixteen.lookslikes.create!(name: 'Wade Phillips',
+looks_like: 'Looks like someone you would find on a bottle of BBQ Sauce.',
+photo_url: 'http://i.imgur.com/N2V2qs0.jpg')
+
+sixteen.lookslikes.create!(name: 'Roy Williams',
+looks_like: 'Looks like the curator of a small-town historical society who patiently gazes out the window for potential visitors all day, sighs, then flips the sign to "CLOSED."',
+photo_url: 'http://i.imgur.com/xZgZ7K2.jpg')
+
+sixteen.lookslikes.create!(name: 'Dana Holgorsen',
+looks_like: 'Looks like that guy your Dad punched at Steak N\' Shake when you were a kid.',
+photo_url: 'http://i.imgur.com/UO7jF00.jpg')
+
+sixteen.lookslikes.create!(name: 'Barry Melrose',
+looks_like: 'Looks like a former gladiator who won his freedom and now runs a gladiator ring of his own in a backwater town.',
+photo_url: 'http://i.imgur.com/VR3F614.jpg')
+
+sixteen.lookslikes.create!(name: 'Kurt Warner',
+looks_like: 'Looks like the man emerging from the pool in the "after" portion of the Rogaine ads.',
+photo_url: 'http://i.imgur.com/nwyJ7w0m.jpg')
+
+sixteen.lookslikes.create!(name: 'Steve Kerr',
+looks_like: 'Looks like the new young pastor at a church that slowly wins over the old people who don\'t like change.',
+photo_url: 'http://i.imgur.com/mDDUhz6m.jpg')
+
+sixteen.lookslikes.create!(name: 'Jeff Fisher',
+looks_like: 'Looks like a guy in a Lynyrd Skynyrd shirt shotgunning a beer on the infield of a NASCAR track.',
+photo_url: 'http://i.imgur.com/En7rkhgl.jpg')
+
+sixteen.lookslikes.create!(name: 'Chris Kaman',
+looks_like: 'Looks like the butler of a haunted mansion.',
+photo_url: 'http://i.imgur.com/iNSgNw4l.jpg')
+
+sixteen.lookslikes.create!(name: 'Ned Yost',
+looks_like: 'Looks like every white person\'s aunt.',
+photo_url: 'http://i.imgur.com/QuEvRATl.jpg')
+
+sixteen.lookslikes.create!(name: 'Charlie Strong',
+looks_like: 'Looks like his name is Charlie Strong.',
+photo_url: 'http://i.imgur.com/M6LZCC6l.jpg')
+
+sixteen.lookslikes.create!(name: 'Roger Goodell',
+looks_like: 'Looks like the pastor in a town that has banned dancing.',
+photo_url: 'http://i.imgur.com/XqtsPcMl.jpg')
+
+sixteen.lookslikes.create!(name: 'Billy Donovan',
+looks_like: 'Looks like a vampire who woke up in the wrong century.',
+photo_url: 'http://i.imgur.com/8Zchmt0l.jpg')
+
+sixteen.lookslikes.create!(name: 'Jim Harbaugh',
+looks_like: 'Looks like the guy who yells at his wife in front of everyone when they lose at Pictionary on game night.',
+photo_url: 'http://i.imgur.com/7rpS86Sl.jpg')
+
+sixteen.lookslikes.create!(name: 'Romeo Crennel',
+looks_like: 'Looks like he just watched his neighbor\'s dog get eaten by an alligator.',
+photo_url: 'http://i.imgur.com/ew7tvcyl.jpg')
+
+sixteen.lookslikes.create!(name: 'Jeff Van Gundy',
+looks_like: 'Looks like a nervous scientist in a Sci-Fi movie who, after a series of awkward exchanges with the hero, says "I don\'t get out much."',
+photo_url: 'http://i.imgur.com/9WvujN2l.jpg')
+
+sixteen.lookslikes.create!(name: 'Jim Tomsula',
+looks_like: 'Sounds like a monster a creole grandma comes up with to keep kids quiet. "Keep quiet or that Jim Tomsula is comin out tha swamp!"',
+photo_url: 'http://i.imgur.com/HVCOD1g.jpg')
+
+sixteen.lookslikes.create!(name: 'Jim Tomsula',
+looks_like: 'Looks like the first guy a newly separated mom goes on a date with.',
+photo_url: 'http://i.imgur.com/pamvYh8.jpg')
+
+sixteen.lookslikes.create!(name: 'Bo Ryan',
+looks_like: 'Looks like the creepy funeral home director who offers recent widows a shoulder to cry on in an attempt to seduce them.',
+photo_url: 'http://i.imgur.com/hyUqJL4.jpg')
+
+sixteen.lookslikes.create!(name: 'Jeremy Shockey',
+looks_like: 'Looks like what would happen if Ed Hardy started making people.',
+photo_url: 'http://i.imgur.com/a4lFADo.jpg')
+
+sixteen.lookslikes.create!(name: 'Jim Tomsula',
+looks_like: 'Looks like the guy riding a wave of water on a float after his above ground pool breaks on America\'s Funniest Home Videos.',
+photo_url: 'http://i.imgur.com/rNzHI06.jpg')
+
+sixteen.lookslikes.create!(name: 'Jim Tomsula',
+looks_like: 'Looks like a construction worker in an action film shouting "you can\'t go that way!" as a car blows through a barricade.',
+photo_url: 'http://i.imgur.com/ieSDnks.jpg')
+
+sixteen.lookslikes.create!(name: 'Bret Bielema',
+looks_like: 'Looks like the guy who says "diet starts tomorrow" as he takes a giant bite of a pulled pork sandwich.',
+photo_url: 'http://i.imgur.com/IKU2xFU.jpg')
+
+sixteen.lookslikes.create!(name: 'Jim Tomsula',
+looks_like: 'Looks like an 80s Olympics bronze medalist shot putter.',
+photo_url: 'http://i.imgur.com/DgSYnf4.jpg')
+
+sixteen.lookslikes.create!(name: 'Steve Kerr',
+looks_like: 'Looks like the guy who brushes his teeth in the work bathroom.',
+photo_url: 'http://i.imgur.com/am0hh5m.jpg')
+
+sixteen.lookslikes.create!(name: 'Joe Buck',
+looks_like: 'Looks like the guy who stops you mid conversation, looks at his phone, and says "I gotta take this, it\'s the Tampa office."',
+photo_url: 'http://i.imgur.com/tcNqgtI.png')
+
+sixteen.lookslikes.create!(name: 'Pablo Torre',
+looks_like: 'Looks like part of the the oddly diverse group of friends pictured in a community college brochure.',
+photo_url: 'http://i.imgur.com/SN534hq.jpg')
+
+sixteen.lookslikes.create!(name: 'Gary Kubiak',
+looks_like: 'Looks like the guy behind the counter at Pep Boys who\'s trying to convince you that you need four new tires.',
+photo_url: 'http://i.imgur.com/catDsjH.jpg')
+
+sixteen.lookslikes.create!(name: 'Mike Tomlin',
+looks_like: 'Looks like a courtroom TV judge that has a catch phrase, and uses it outside of work to try to get recognized.',
+photo_url: 'http://i.imgur.com/T9Xqz14.jpg')
+
+sixteen.lookslikes.create!(name: 'Jim Tomsula',
+looks_like: 'Looks like the guy that brings doughnuts to work to share, then eats 90 percent of them.',
+photo_url: 'http://i.imgur.com/wX36xYs.jpg')
+
+sixteen.lookslikes.create!(name: 'Jimmy Graham',
+looks_like: 'Looks like a default created player in a video game.',
+photo_url: 'http://i.imgur.com/1RY2rkA.jpg')
+
+sixteen.lookslikes.create!(name: 'Stan Kroenke',
+looks_like: 'Looks like a character from the Beastie Boys "Sabotage" video.',
+photo_url: 'http://i.imgur.com/igqna97.jpg')
+
+sixteen.lookslikes.create!(name: 'Jim Tomsula',
+looks_like: 'Looks like the general manager for a regional bologna distributor.',
+photo_url: 'http://i.imgur.com/w1pZmIW.jpg')
+
+sixteen.lookslikes.create!(name: 'Jim Tomsula',
+looks_like: 'Looks like the guy cooking up hot dogs at the street stand at 4 am when you get out of the club.',
+photo_url: 'http://i.imgur.com/VQWdBd6.jpg')
+
+sixteen.lookslikes.create!(name: 'Wade Phillips',
+looks_like: 'Looks like a flabbergasted Boy Scout troop leader in the woods, who can\'t find his way back to camp.',
+photo_url: 'http://i.imgur.com/XdguQYh.jpg')
+
+sixteen.lookslikes.create!(name: 'Joey Crawford',
+looks_like: 'Looks like an animated turtle walking around without his shell.',
+photo_url: 'http://i.imgur.com/r96cyLt.jpg')
+
+sixteen.lookslikes.create!(name: 'Teddy Bridgewater',
+looks_like: 'Sounds like a children\'s book about a stuffed animal saving a town from a flood.',
+photo_url: 'http://i.imgur.com/EJmVyLj.jpg')
+
+sixteen.lookslikes.create!(name: 'Buck Showalter',
+looks_like: 'Looks like the old Texas lawman who squashes disputes at the local saloon by simply swinging open the double doors and staring down rowdy patrons. Richie Incognito and Ndamukong Suh are those rowdy patrons.',
+photo_url: 'http://i.imgur.com/ZglkEVh.jpg')
+
+sixteen.lookslikes.create!(name: 'Urban Meyer',
+looks_like: 'Looks like the guy that has to have a bigger lawnmower than his next door neighbor.',
+photo_url: 'http://i.imgur.com/SvthT2U.jpg')
+
+sixteen.lookslikes.create!(name: 'Chuck Pagano',
+looks_like: 'Looks like Tony Soprano\'s friend from childhood that Tony has to reluctantly beat up for repeatedly failing to pay back a gambling debt. Bruce Arians and Mike Dunleavy Sr. are the two guys on lookout while Tony Soprano is beating up Chuck Pagano.',
+photo_url: 'http://i.imgur.com/WxrLWL2.jpg')
+
+sixteen.lookslikes.create!(name: 'Tim Legler',
+looks_like: 'Looks like the guy from the razor commercials who strokes his face with delight.',
+photo_url: 'http://i.imgur.com/JYF1Vqb.jpg')
+
+sixteen.lookslikes.create!(name: 'Mark Cuban',
+looks_like: 'Looks like a Roman senator who conspired against Caesar.',
+photo_url: 'http://i.imgur.com/KwH2ZLy.jpg')
+
+sixteen.lookslikes.create!(name: 'Art Briles',
+looks_like: 'Looks like an old man sitting on a rocking chair outside a bar who pulls out a gun and sets it on his lap while telling people "not from around these parts" asking for directions, "yeah, it\'s back the way you came."',
+photo_url: 'http://i.imgur.com/TIZYIOj.jpg')
+
+sixteen.lookslikes.create!(name: 'Brad Nessler',
+looks_like: 'Looks like he walked out of a 1920s gangster-run nightclub.',
+photo_url: 'http://i.imgur.com/QYA5qZv.jpg')
+
+sixteen.lookslikes.create!(name: 'Les Miles',
+looks_like: 'Looks like a judge for your local chili cook-off who puts his finger in your bowl and says, "it needs a little more heat."',
+photo_url: 'http://i.imgur.com/HkeiLhC.jpg')
+
+sixteen.lookslikes.create!(name: 'Rick Fox',
+looks_like: 'Looks like the beach lifeguard who is too busy flirting with girls to notice a child drifting out to sea.',
+photo_url: 'http://i.imgur.com/P0HwGBW.jpg')
+
+sixteen.lookslikes.create!(name: 'Scott Van Pelt',
+looks_like: 'Looks like a computer-generated image of what we used to think people would look like in the future.',
+photo_url: 'http://i.imgur.com/u03EDya.jpg')
+
+sixteen.lookslikes.create!(name: 'Bob Ley',
+looks_like: 'Looks like the guy who, after being told a secret, pretends to zip his lips, lock them up, and throw away the key over his shoulder.',
+photo_url: 'http://i.imgur.com/CoDKMgH.jpg')
+
+sixteen.lookslikes.create!(name: 'Stan Van Gundy',
+looks_like: 'Looks like the guy who gets into a hot tub and is unaware that his shorts are quickly filling up with air.',
+photo_url: 'http://i.imgur.com/cytkkdF.jpg')
+
+sixteen.lookslikes.create!(name: 'Jameis Winston',
+looks_like: 'Looks like a cartoon dinosaur.',
+photo_url: 'http://i.imgur.com/ZvxPFae.jpg')
